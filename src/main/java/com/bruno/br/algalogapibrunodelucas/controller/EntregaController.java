095a8c5c0a7736d31ebed778ff29a52f322b139f
brunodelucasbarbosa <brunnobarbosas@gmail.com>
2022-05-21 16:32:14 -0300

    entrega service

--- src/main/java/com/bruno/br/algalogapibrunodelucas/controller/EntregaController.java
+++ src/main/java/com/bruno/br/algalogapibrunodelucas/controller/EntregaController.java
@@ -1,10 +1,24 @@
 package com.bruno.br.algalogapibrunodelucas.controller;
 
 
+import com.bruno.br.algalogapibrunodelucas.model.Entrega;
+import com.bruno.br.algalogapibrunodelucas.services.SolicitacaoEntregaService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class EntregaController {
+
+  private SolicitacaoEntregaService solicitacaoEntregaService;
+
+  @PostMapping("/entregas")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Entrega criar(@RequestBody Entrega entrega) {
+    return solicitacaoEntregaService.solicitar(entrega);
+  }
 }
